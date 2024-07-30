37b4e3dabd9045f2cb0d5aee90b1f9a93675d121
1sakib <146053153+1sakib@users.noreply.github.com>
2024-07-29 23:07:24 -0400

    test stuff

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const { innerHeight } = window;
 
-// zoom-out
-gsap.from("#zoom-out h2", {
-    scale: 50, stagger: 0.25, duration: 3,
-    scrollTrigger: {
-        trigger: "#zoom-out",
-        pin: true,
-        end: `+=${innerHeight * 1.3}`,
-        scrub: 3
-    }
-});
 
 // zoom-in
 gsap.to("#zoom-in h2", {
-    scale: 100, stagger: 0.25, duration: 3,
-    scrollTrigger: {
-        trigger: "#zoom-in",
-        pin: true,
-        end: `+=${innerHeight * 1.3}`,
-        scrub: 3
-    }
+    scale:25,
+    x:500,
+    y: 250,
+    duration: 1,
+    scrollTrigger: ".test",
+    
 });
+
